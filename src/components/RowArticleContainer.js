0810d072e50720fe518723a2b52150a9366e2cad
vertunoff <anton.s.vertunov@gmail.com>
2023-12-08 00:38:38 +0300

    fixed wrong articleblocks

--- src/components/RowArticleContainer.js
+++ src/components/RowArticleContainer.js
@@ -4,20 +4,24 @@ import LeftArticle from "./LeftArticle";
 import RightArticle from "./RightArticle";
 
 const RowArticleContainer = (props) => {
-    if (props.reversed == 'false') {
+    if (props.reversed === 'false') {
         return (
             <div className="row-article-container">
                 <LeftArticle src={props.data[0].photo} title={props.data[0].title} topic={props.data[0].topic}></LeftArticle>
-                <RightArticle src={props.data[1].photo} title={props.data[1].title} topic={props.data[1].topic}></RightArticle>
-                <RightArticle src={props.data[2].photo} title={props.data[2].title} topic={props.data[2].topic}></RightArticle>
+                <div className="right-article-containers">
+                    <RightArticle src={props.data[1].photo} title={props.data[1].title} topic={props.data[1].topic}></RightArticle>
+                    <RightArticle src={props.data[2].photo} title={props.data[2].title} topic={props.data[2].topic}></RightArticle>
+                </div>
             </div>
         )
     }
-    else if (props.reversed == 'true') {
+    else if (props.reversed === 'true') {
         return (
             <div className="row-article-container">
-                <RightArticle src={props.data[0].photo} title={props.data[0].title} topic={props.data[0].topic}></RightArticle>
-                <RightArticle src={props.data[1].photo} title={props.data[1].title} topic={props.data[1].topic}></RightArticle>
+                <div className="right-article-containers">
+                    <RightArticle src={props.data[0].photo} title={props.data[0].title} topic={props.data[0].topic}></RightArticle>
+                    <RightArticle src={props.data[1].photo} title={props.data[1].title} topic={props.data[1].topic}></RightArticle>
+                </div>
                 <LeftArticle src={props.data[2].photo} title={props.data[2].title} topic={props.data[2].topic}></LeftArticle>
 
             </div>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import MainPage from './components/MainPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MainPage />
   </React.StrictMode>
 );
 
